0dc32d2b6bfe76ebcb379bce5496470cabe163f6
steff34 <steffen.p.wiers@gmail.com>
2020-03-13 23:05:21 +0100

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "ppca-tool",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Steffen Wiers",
     author_email = "steffen.p.wiers@gmail.com",
     description = "Tool to perform principal component analysis on palaeomagnetic data sets",
